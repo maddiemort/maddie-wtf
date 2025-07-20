b6fb0235d5168c4dd054d18036e64dc770eda4fa
Madeleine Mortensen <me@maddie.wtf>
2025-07-20 13:17:30 +0100

    feat: always format dates in a human-readable way

--- src/state/render.rs
+++ src/state/render.rs
@@ -19,14 +19,18 @@ impl Render for PostRef<'_> {
     fn render(&self) -> Markup {
         match self.guard.deref() {
             post @ Post::Single {
-                metadata,
+                metadata: _,
                 html_summary: _,
                 html_content,
             } => html! {
                 article {
                     (PreEscaped(post.html_title(1)))
                     ul class="frontmatter" {
-                        li { (metadata.date) }
+                        li {
+                            time datetime=(post.date_posted()) {
+                                (post.date_posted().format("%e %B %Y"))
+                            }
+                        }
                         @for tag in post.tags() {
                             li {
                                 // a href=(format!("/tagged/{}", tag)) {
